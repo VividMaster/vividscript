306ae69da88d15e71c91ad3f0edff285c475b29c
Anthony Robert Wells <scopedynamo@gmail.com>
2017-11-15 18:04:00 +0000

    initial commit
    balls rolling.

---
+++ VToken.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VividScript
+{
+    public enum TokenClass
+    {
+        Type,Value,Statement,Define,Op
+    }
+    public enum Token
+    {
+        If,Else,End,Module,Method,Func,Equal,Plus,Minus,Multi,Div,Comma,Peroid,Colon,SemiColon,StringMark,Int,Short,Byte,Long,Float,Double,String,
+        Var,Transient,State,Auto,Link,Pow
+    }
+    public class VToken
+    {
+        public TokenClass Class;
+        public Token Token;
+        public string Text = "";
+        public string String = "";
+        public byte BVal;
+        public short SVal;
+        public int IVal;
+        public long LVal;
+        public float FVal;
+        public double DVal;
+        public VToken(TokenClass cls,Token tok,string txt)
+        {
+            Class = cls;
+            Token = tok;
+            Text = txt;
+        }
+
+    }
+    public class VTokenStream
+    {
+        public List<VToken> Tokes = new List<VToken>();
+        public int Pos = 0;
+        public int Len = 0;
+        public VToken Get()
+        {
+            return Tokes[Pos];
+        }
+        public VToken GetNext()
+        {
+            return Tokes[Pos++];
+        }
+        public VToken Find(Token t)
+        {
+            foreach(var ti in Tokes)
+            {
+                if (ti.Token == t) return ti;
+            }
+            return null;
+        }
+        public VToken FindPrev(Token t)
+        {
+            for(int ti=Pos;ti>=0;ti--)
+            {
+                if (Tokes[ti].Token == t) return Tokes[ti];
+            }
+            return null;
+        }
+        public VToken FindNext(Token t)
+        {
+            for(int ti = Pos; ti < Len; ti++)
+            {
+                if (Tokes[ti].Token == t) return Tokes[ti];
+            }
+            return null;
+        }
+    }
+}
---
+++ VTokenizer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VividScript
+{
+    public class VTokenizer
+    {
+        public List<string> Ops = new List<string>();
+        public List<string> Types = new List<string>();
+        public List<string> Keys = new List<string>();
+        public Dictionary<string, VToken> ConvTable = new Dictionary<string, VToken>();
+        public void AddConv(string tok,VToken t)
+        {
+            ConvTable[tok] = t;
+        }
+        public VTokenizer()
+        {
+           
+            Ops.Add("+");
+            Ops.Add("-");
+            Ops.Add("/");
+            Ops.Add("*");
+            Ops.Add("^");
+            Ops.Add("mod");
+            Ops.Add("==");
+            AddConv("+", new VToken(TokenClass.Op, Token.Plus, "+"));
+            AddConv("-", new VToken(TokenClass.Op, Token.Minus, "-"));
+            AddConv("/", new VToken(TokenClass.Op, Token.Div, "/"));
+            AddConv("*", new VToken(TokenClass.Op, Token.Div, "*"));
+            AddConv("^", new VToken(TokenClass.Op, Token.Pow, "^"));
+            AddConv("mod", new VToken(TokenClass.Op, Token.Pow, "mod"));
+            AddConv("==", new VToken(TokenClass.Op, Token.Equal, "=="));
+
+            Types.Add("byte");
+            Types.Add("short");
+            Types.Add("int");
+            Types.Add("long");
+            Types.Add("float");
+            Types.Add("double");
+            Types.Add("string");
+
+            Keys.Add("if");
+            Keys.Add("elseif");
+            Keys.Add("end");
+            Keys.Add("for");
+            Keys.Add("next");
+            Keys.Add("while");
+            Keys.Add("module");
+            Keys.Add("method");
+            Keys.Add("function");
+
+
+        }
+    }
+}
